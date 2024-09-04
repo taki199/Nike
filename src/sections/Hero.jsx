c543650c81c7051e86c862e42e637036222ce7f1
taki199 <christotaki22@gmail.com>
2024-09-04 18:04:30 +0200

    hero section modifications

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -53,7 +53,7 @@ const Hero = () => {
           alt='shoe colletion'
           width={610}
           height={502}
-          className='object-contain relative z-10'
+          className='object-contain relative z-10 animate-bounce'
         />
 
         <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
